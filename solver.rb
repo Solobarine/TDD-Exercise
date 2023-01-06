53d98b506c1298cdd98504b71d4317afaa0965d7
Shedrack-Sunday <hannania002@gmail.com>
2023-01-06 19:51:29 +0100

    Fix linter errors

--- solver.rb
+++ solver.rb
@@ -1,28 +1,28 @@
 class Solver
   def factorial(num)
-    if num < 0
-      raise RuntimeError, 'RuntimeError'
-    elsif (num == 0 or num == 1)
-      return 1
+    if num.negative?
+      raise 'RuntimeError'
+    elsif [0, 1].include?(num)
+      1
     else
-      n = num * factorial(num-1)
-      return n
+      num * factorial(num - 1)
+
     end
   end
 
   def reverse(word)
-    return word.downcase.reverse
+    word.downcase.reverse
   end
 
   def fizzbuzz(num)
-    if num % 3 == 0 && num % 5 == 0
-      return 'fizzbuzz'
-    elsif num % 3 == 0
-      return 'fizz'
-    elsif num % 5 == 0
-      return 'buzz'
+    if (num % 3).zero? && (num % 5).zero?
+      'fizzbuzz'
+    elsif (num % 3).zero?
+      'fizz'
+    elsif (num % 5).zero?
+      'buzz'
     else
-      return num.to_s
+      num.to_s
     end
   end
 end
--- solver_spec.rb
+++ solver_spec.rb
@@ -1,18 +1,18 @@
-require "./solver"
-require "rspec"
+require './solver'
+require 'rspec'
 
 RSpec.describe Solver do
   subject(:solver) { described_class.new }
 
-  describe "#factorial" do
-    context "with negative input" do
-      it "raises an error" do
+  describe '#factorial' do
+    context 'with negative input' do
+      it 'raises an error' do
         expect { solver.factorial(-1) }.to raise_error(RuntimeError)
       end
     end
 
-    context "with positive input" do
-      it "returns the correct factorial" do
+    context 'with positive input' do
+      it 'returns the correct factorial' do
         expect(solver.factorial(0)).to eq(1)
         expect(solver.factorial(1)).to eq(1)
         expect(solver.factorial(2)).to eq(2)
@@ -22,20 +22,20 @@ RSpec.describe Solver do
     end
   end
 
-  describe "#reverse" do
-    it "returns the reversed string" do
-      expect(solver.reverse("hello")).to eq("olleh")
-      expect(solver.reverse("world")).to eq("dlrow")
-      expect(solver.reverse("apple")).to eq("elppa")
+  describe '#reverse' do
+    it 'returns the reversed string' do
+      expect(solver.reverse('hello')).to eq('olleh')
+      expect(solver.reverse('world')).to eq('dlrow')
+      expect(solver.reverse('apple')).to eq('elppa')
     end
   end
 
-  describe "#fizzbuzz" do
-    it "returns the correct string based on the rules" do
-      expect(solver.fizzbuzz(3)).to eq("fizz")
-      expect(solver.fizzbuzz(5)).to eq("buzz")
-      expect(solver.fizzbuzz(15)).to eq("fizzbuzz")
-      expect(solver.fizzbuzz(7)).to eq("7")
+  describe '#fizzbuzz' do
+    it 'returns the correct string based on the rules' do
+      expect(solver.fizzbuzz(3)).to eq('fizz')
+      expect(solver.fizzbuzz(5)).to eq('buzz')
+      expect(solver.fizzbuzz(15)).to eq('fizzbuzz')
+      expect(solver.fizzbuzz(7)).to eq('7')
     end
   end
 end
